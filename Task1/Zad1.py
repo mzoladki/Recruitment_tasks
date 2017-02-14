cd3118f5a829459bead37821c7d56377907b768d
mzoladki <zoladkiewiczmarcin@gmail.com>
2017-02-14 15:30:31 +0100

    Add files via upload

---
+++ Task1/Zad1.py
@@ -0,0 +1,44 @@
+def fight(army, even, odd):
+    for soldier in army:
+        soldier = "{0:b}".format(soldier)
+        even_counter = 0
+        odd_counter = 0
+        if int(soldier) % 2 == 1:
+            for a in soldier:
+                if a == "1":
+                    odd_counter += 1
+            if soldier[0] == "-":
+                odd.append(int(odd_counter) * (-1))
+            else:
+                odd.append(int(odd_counter))
+        else:
+            for a in soldier:
+                if a == "0":
+                    even_counter += 1
+            if soldier[0] == "-":
+                even.append(int(even_counter) * (-1))
+            else:
+                even.append(int(even_counter))
+
+
+def result(even, odd):
+    odd_strength = 0
+    even_strength = 0
+    for a in even:
+        even_strength += a
+    for a in odd:
+        odd_strength += a
+    if even_strength > odd_strength:
+        print("evens win")
+    elif even_strength < odd_strength:
+        print("odds win")
+    else:
+        print("tie")
+
+
+battle = [23, -3]
+even_stats = []
+odd_stats = []
+
+fight(battle, even_stats, odd_stats)
+result(even_stats, odd_stats)
